6ec46d06ee1422114e6672007642b59fe0cb6193
Maciej Słupianek <maciej.slupianekge@gmail.com>
2023-06-24 22:16:03 +0200

    front-set-up

---
+++ src/components/Navbar.vue
@@ -0,0 +1,91 @@
+<template>
+    <div class="div__nav">
+      <div class='div__icons decoy'>
+        <v-icon class='icon' icon='mdi-facebook'></v-icon>
+        <v-icon class='icon' icon='mdi-twitter'></v-icon>
+      </div>
+      <div class='div__links'>
+        <span @click.capture="onClickedHome">
+          <router-link class='link home' :style="{'color': clicked === 'homeClicked' ? '#8484d8' : '#c7c7c7'}" to="/">Home</router-link> 
+        </span>
+        <span>
+          <router-link @click="onClickedAbout" class='link about' :style="{'color': clicked === 'aboutClicked' ? '#8484d8' : '#c7c7c7'}" to="/about">About</router-link>
+        </span>
+        <span>
+          <router-link @click="onClickedCatalog" class='link catalog' :style="{'color': clicked === 'catalogClicked' ? '#8484d8' : '#c7c7c7'}" to="/catalog">Catalog</router-link> 
+        </span>
+      </div>
+      <div class='div__icons'>
+        <v-icon class='icon' icon='mdi-facebook'></v-icon>
+        <v-icon class='icon' icon='mdi-twitter'></v-icon>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+             clicked:'homeClicked' 
+        }
+    },
+    methods: {
+      onClickedHome() {
+          this.clicked = 'homeClicked'
+      },
+      onClickedAbout() {
+          this.clicked = 'aboutClicked'
+      },
+      onClickedCatalog() {
+          this.clicked = 'catalogClicked'
+      }
+    }
+
+}
+</script>
+
+<style scoped lang='scss'>
+.div__nav {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  .div__links {
+    display: flex;
+    align-items:baseline;
+    .link {
+    text-decoration: none;
+    color: rgb(199, 199, 199);
+    font-size: 40px;
+    padding: 0 20px 0 20px;
+    transition: 0.2s ease-in-out;
+    &.home {
+      font-size: 60px;
+    }
+    &:hover {
+      color: rgb(132, 132, 216);
+    }
+    &.home:hover {
+      color: rgb(94, 94, 201);
+    }
+  }
+  }
+  .div__icons {
+    display: flex;
+    align-items: center;
+    &.decoy {
+      visibility: hidden;
+    }
+    .icon {
+      font-size: 40px;
+      margin: 0 5px 0 5px;
+      transition: 0.2s ease-in-out;
+      color: rgb(199, 199, 199);
+      cursor: pointer;
+      &:hover {
+        color: rgb(132, 132, 216);
+        scale: 1.2;
+      }
+    }
+  }
+}
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,48 @@
+<template lang="">
+    <div class='div__about'>
+        <span class='span__header'>
+            Sales Manager App
+        </span>
+        <span class='span__description'>
+            With this app u can control what u have in your store, in 
+            our case <b> Houses for animals </b>. You can sort, filter, 
+            and search for all types of houses. You can also add new house 
+            with category, animal and price of your choice. Every new cataegory will be
+            added to the the sort and filter options. You can also delete any house u want.
+        </span>
+        <span class='span__signature'>
+            Page created by <span class='name'> Maciej Słupianek </span>
+        </span>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.div__about {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 15vh;
+    .span__header {
+        font-size: 60px;
+        font-weight: 600;
+        margin-bottom: 20px;
+        color: rgb(132, 132, 216);
+    }
+    .span__description {
+        font-size: 30px;
+        font-weight: 400;
+        margin-bottom: 20px;
+        text-align: center;
+        width: 600px;
+    }
+    .span__signature {
+        font-size: 20px;
+        font-weight: 400;
+        margin-bottom: 20px;
+        .name {
+            font-weight: 600;
+            color: rgb(132, 132, 216);
+        }
+    }
+}
+</style>
